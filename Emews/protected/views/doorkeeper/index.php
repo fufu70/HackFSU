c37b6ded6bd490774603a15168aefd4661f5d04a
Christian Micklisch <cmicklis@stetson.edu>
2014-04-06 10:17:40 -0400

    fdas

--- Emews/protected/views/doorkeeper/index.php
+++ Emews/protected/views/doorkeeper/index.php
@@ -21,10 +21,6 @@
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
-      var urlArray = location.href;
-      var newURL = urlArray + 'shit.php';
-      location.href = newURL;
-      document.getElementById("fuck").innerHTML = "fuck";
       testAPI();
     } else if (response.status === 'not_authorized') {
       // In this case, the person is logged into Facebook, but not into the app, so we call
